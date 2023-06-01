8c8262c878de9a6ba696761de87efa8463558ba7
Brook Riggio <brookr@hey.com>
2023-06-01 00:59:44 -0700

    Updates the home page with pillars of OM (#45)

---
+++ web/src/components/Autotyping.tsx
@@ -0,0 +1,28 @@
+// components/AutoTyping.tsx
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+
+interface AutoTypingProps {
+  strings: string[];
+}
+
+const AutoTyping: React.FC<AutoTypingProps> = ({ strings }) => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings,
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, [strings]);
+
+  return <span ref={el} />;
+};
+
+export default AutoTyping;
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/Home.module.scss";
 import { Col, Container, Row } from "react-bootstrap";
 import dynamic from "next/dynamic";
 import HeaderPlaceholder from "@/components/header/HeaderPlaceholder";
+import AutoTyping from "@/components/AutoTyping";
 
 const Header = dynamic(() => import("../components/header/Header"), {
   ssr: false,
@@ -26,58 +27,59 @@ export default function Home() {
         <Row
           className="d-flex align-items-center pb-5">
           <Col
-            md={8}
             className="font-larger">
             <main>
-              <div className="pt-5 pb-5">
-                <h1>The OM Pub</h1>
-                <div className="text-subheading pt-3">Experiments in decentralization</div>
-              </div>
+              <Row className={`${styles.hero}`}>
+                <h1>
+                  A future that is<br />{' '}
+                  <AutoTyping strings={['digital', 'open', 'shared', 'decentralized', 'free', 'self-sovereign', 'communal']} />
+                </h1>
+              </Row>
+              <Row className={`${styles.welcome}`}>
+                <h3>
+                  Welcome to the OM Pub! 
+                </h3>
+                <p>
+                Our goal is to promote and spread knowledge of the key concepts to an open metaverse. 
+                These include equitable access, data ownership, digital rights, and the freedom to transact. 
+                Join us in these experiments in decentralization and sharing a vision of the future that is open to all. 
+                </p>
+              </Row>
+              <Row className={`${styles.equitable}`}>
+                <h2 className={`${styles.pillar}`}>
+                  Equitable Access
+                </h2>
+              </Row>
+              <Row className={`${styles.ownership}`}>
+                <h2 className={`${styles.pillar}`}>
+                  Data Ownership
+                </h2>
+              </Row>
+              <Row className={`${styles.rights}`}>
+                <h2 className={`${styles.pillar}`}>
+                  Digital Rights
+                </h2>
+              </Row>
+              <Row className={`${styles.freedom}`}>
+                <h2 className={`${styles.pillar}`}>
+                  Freedom to Transact
+                </h2>
+              </Row>
               <article>
+                <div className="text-subheading pt-3">Experiments in decentralization</div>
+
                 <h3>
                   It's time.
                 </h3>
                 <p>
                   We need pathways to an open metaverse that runs on publicly accessible protocols, not corporate servers.
-                  Join us, in these experiments of decentralization and sharing a vision of the future open to all.
+                  Join us, in these experiments of decentralization and sharing a vision of digital ownership and access, open to all.
                 </p>
-                <h4>
-                  Currently brewing at the OM Pub...
-                </h4>
-                <h5>The Memes by 6529: Prememes</h5>
-                <ul>
-                  <li>Season 3</li>
-                  <ul>
-                    <li>Artist Preview: <a href="/memes/106/artist">Meme Artist 106 - ICKI / noCreative</a></li>
-                    <li>Artist Preview: <a href="/memes/105/artist">Meme Artist 105 - José Ramos</a></li>
-                    <li>Artist Preview: <a href="/memes/104/artist">Meme Artist 104 - Timpers</a></li>
-                    <li>Artist Preview: <a href="/memes/103/artist">Meme Artist 103 - Jack Butcher</a></li>
-                    <li>Artist Preview: <a href="/memes/102/artist">Meme Artist 102 - GxngYxng</a></li>
-                    <li>Artist Preview: <a href="/memes/101/artist">Meme Artist 101 - LordJamieVShiLL</a></li>
-                    <li>Artist Preview: <a href="/memes/100/artist">Meme Artist 100 - 6529er</a></li>
-                    <li>Artist Preview: <a href="/memes/99/artist">Meme Artist 99 - Mike Hirshon</a></li>
-                    <li>Artist Preview: <a href="/memes/98/artist">Meme Artist 98 - Degen Alfie</a></li>
-                    <li>Artist Preview: <a href="/memes/97/artist">Meme Artist 97 - Matt Doogue</a></li>
-                    <li>Artist Preview: <a href="/memes/96/artist">Meme Artist 96 - Billy Dinh</a></li>
-                    <li>Artist Preview: <a href="/memes/95/artist">Meme Artist 95 - Andreas Preis</a></li>
-                    <li>Artist Preview: <a href="/memes/94/artist">Meme Artist 94 - Jeff Soto</a></li>
-                    <li>Artist Preview: <a href="/memes/93/artist">Meme Artist 93 - mbsjq</a></li>
-                    <li>Artist Preview: <a href="/memes/92/artist">Meme Artist 92 - Angela Nikolau</a></li>
-                    <li>Artist Preview: <a href="/memes/90/artist">Meme Artist 90 - Meg Thorpe</a></li>
-                    <li>Artist Preview: <a href="/memes/89/artist">Meme Artist 89 - CyptoClimates: Made by Megan</a></li>
-                    <li>Artist Preview: <a href="/memes/88/artist">Meme Artist 88 - Ryan Koopmans</a></li>
-                    <li>Artist Preview: <a href="/memes/87/artist">Meme Artist 87 - DeeKay Motion</a></li>
-                  </ul>
-                  <li>Season 2</li>
-                  <ul>
-                    <li>Artist Preview: <a href="/memes/86/artist">Meme Artist 86 - Pop Wonder</a></li>
-                    <li>Artist Preview: <a href="/memes/85/artist">Meme Artist 85 - 6529er</a></li>
-                    <li>Artist Preview: <a href="/memes/83/artist">Meme Artist 83 - Luna Leonis</a></li>
-                  </ul>
-                </ul>
               </article>
             </main>
           </Col>
+        </Row>
+        <Row>
           <Col
             md={{ span: 3, offset: 1 }}
             className="d-flex justify-content-end">
@@ -93,7 +95,7 @@ export default function Home() {
             </aside>
           </Col>
         </Row>
-      </Container>
+      </Container >
     </>
   );
 }
