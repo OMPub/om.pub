e18c6c89120810caf154b0b1842423c77f2057bc
brookr <11095+brookr@users.noreply.github.com>
2024-03-31 09:37:51 -0700

    Time ago just needs days
    Signed-off-by: brookr <11095+brookr@users.noreply.github.com>

--- web/src/services/seizeApi.ts
+++ web/src/services/seizeApi.ts
@@ -352,11 +352,7 @@ const timeAgo = (milliseconds: number): string => {
   const months = Math.floor(days / 30);
   const years = Math.floor(months / 12);
 
-  if (years > 0) {
-    return `${years} year${years > 1 ? "s" : ""} ago`;
-  } else if (months > 0) {
-    return `${months} month${months > 1 ? "s" : ""} ago`;
-  } else if (days > 0) {
+  if (days > 0) {
     return `${days} day${days > 1 ? "s" : ""} ago`;
   } else if (hours > 0) {
     return `${hours} hour${hours > 1 ? "s" : ""} ago`;
