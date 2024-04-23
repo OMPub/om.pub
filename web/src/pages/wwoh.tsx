f7f3a744296e140bb4df8502914e787dd583d1b1
brookr <11095+brookr@users.noreply.github.com>
2024-04-23 10:03:53 -0700

    Adds sidebar to wwoh story
    Signed-off-by: brookr <11095+brookr@users.noreply.github.com>

--- web/src/pages/wwoh.tsx
+++ web/src/pages/wwoh.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import dynamic from "next/dynamic";
 import HeaderPlaceholder from "@/components/header/HeaderPlaceholder";
 import styles from "@/styles/Home.module.scss";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Form } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { fetchMomoReps, timeAgo } from "@/services/seizeApi";
 
@@ -21,6 +21,29 @@ interface StoryLine {
 const StoryPage = () => {
   const [storyLines, setStoryLines] = useState<StoryLine[]>([]);
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+  const [selectedAuthors, setSelectedAuthors] = useState<string[]>([]);
+  const authorsWithCount = storyLines.reduce((acc: any, storyLine) => {
+    acc[storyLine.author] = (acc[storyLine.author] || 0) + 1;
+    return acc;
+  }, {});
+  
+  const sortedAuthors = Object.keys(authorsWithCount).sort();
+  
+  useEffect(() => {
+    const authors = [
+      ...new Set(storyLines.map((storyLine) => storyLine.author)),
+    ];
+    setSelectedAuthors(authors);
+  }, [storyLines]);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const author = event.target.name;
+    if (selectedAuthors.includes(author)) {
+      setSelectedAuthors(selectedAuthors.filter((a) => a !== author));
+    } else {
+      setSelectedAuthors([...selectedAuthors, author]);
+    }
+  };
 
   const sortedStoryLines = [...storyLines].sort((a, b) => {
     if (sortOrder === "asc") {
@@ -30,10 +53,14 @@ const StoryPage = () => {
     }
   });
 
+  const visibleStoryLines = sortedStoryLines.filter((storyLine) =>
+    selectedAuthors.includes(storyLine.author)
+  );
+
   const toggleSortOrder = () => {
     setSortOrder((prevSortOrder) => (prevSortOrder === "asc" ? "desc" : "asc"));
   };
-  
+
   useEffect(() => {
     const getStoryLines = async () => {
       try {
@@ -67,46 +94,76 @@ const StoryPage = () => {
         className={`${styles.main}`}
         style={{ maxWidth: "850px", margin: "auto", padding: "20px" }}
       >
-        <h1>Woman with Orange Hat</h1>
-        <p>
-          Who is WwOH? What is her story? Details are in{" "}
-          <a
-            style={{ textDecoration: "none" }}
-            href="https://discord.com/channels/963043925554237482/1203782401843793971/1212814004255195136"
-            target="_blank"
-          >
-            Proposal #25
-          </a>
-          .
-        </p>
-        <p style={{ overflow: "auto" }}>
-          <button
-            onClick={toggleSortOrder}
-            style={{ float: "right", border: "none", backgroundColor: "#fff", color: "#ccc" }}
-          >
-            {sortedStoryLines.length} / 420 {sortOrder === "asc" ? "🔼" : "🔽"}
-          </button>
-        </p>
-        {sortedStoryLines.map((storyLine) => (
-          <Card className="story-line" style={{ border: "none" }}>
-            <Card.Body>
-              <Card.Text
-                style={{ fontFamily: "Lora, serif", fontSize: "large" }}
+        <Row>
+          <Col>
+            <h1>Woman with Orange Hat</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={3}>
+            {Array.from(
+              new Set(storyLines.map((storyLine) => storyLine.author))
+            ).map((author) => (
+              <div key={author}>
+                <Form.Check
+                  type="checkbox"
+                  id={`checkbox-${author}`}
+                  name={author}
+                  label={`${author} (${authorsWithCount[author]})`}
+                  checked={selectedAuthors.includes(author)}
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+            ))}
+          </Col>
+          <Col md={9}>
+            <p>
+              Who is WwOH? What is her story? Details are in{" "}
+              <a
+                style={{ textDecoration: "none" }}
+                href="https://discord.com/channels/963043925554237482/1203782401843793971/1212814004255195136"
+                target="_blank"
               >
-                {storyLine.text}
-              </Card.Text>
-              <Card.Subtitle
-                className="mb-2"
-                style={{ textAlign: "right", color: "#ccc" }}
+                Proposal #25
+              </a>
+              .
+            </p>
+            <p style={{ overflow: "auto" }}>
+              <button
+                onClick={toggleSortOrder}
+                style={{
+                  float: "right",
+                  border: "none",
+                  backgroundColor: "#fff",
+                  color: "#ccc",
+                }}
               >
-                by {storyLine.author} at&nbsp;
-                <span title={new Date(storyLine.timestamp).toISOString()}>
-                  {timeAgo(storyLine.timestamp)}
-                </span>
-              </Card.Subtitle>
-            </Card.Body>
-          </Card>
-        ))}
+                {visibleStoryLines.length} / 420{" "}
+                {sortOrder === "asc" ? "🔼" : "🔽"}
+              </button>
+            </p>
+            {visibleStoryLines.map((storyLine) => (
+              <Card className="story-line" style={{ border: "none" }}>
+                <Card.Body>
+                  <Card.Text
+                    style={{ fontFamily: "Lora, serif", fontSize: "large" }}
+                  >
+                    {storyLine.text}
+                  </Card.Text>
+                  <Card.Subtitle
+                    className="mb-2"
+                    style={{ textAlign: "right", color: "#ccc" }}
+                  >
+                    by {storyLine.author} at&nbsp;
+                    <span title={new Date(storyLine.timestamp).toISOString()}>
+                      {timeAgo(storyLine.timestamp)}
+                    </span>
+                  </Card.Subtitle>
+                </Card.Body>
+              </Card>
+            ))}
+          </Col>
+        </Row>
       </Container>
       <style jsx>{`
         .story-line-info {
