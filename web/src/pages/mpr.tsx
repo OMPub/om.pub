ffbf6a07b57f824d052be59f6732e363bae6419f
Brook Riggio <11095+brookr@users.noreply.github.com>
2024-08-15 14:27:16 -0700

    dev (#71)
    * Ensures no emoji line wrap
    
    Signed-off-by: brookr <11095+brookr@users.noreply.github.com>
    
    * Simplifies nowrap for emoji
    
    Signed-off-by: brookr <11095+brookr@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: brookr <11095+brookr@users.noreply.github.com>

--- web/src/pages/mpr.tsx
+++ web/src/pages/mpr.tsx
@@ -332,7 +332,7 @@ const MemeRankPage = () => {
                     <td>{card.rank !== null ? card.rank : "--"}</td>
                     <td
                       title={card.rep.toString()}
-                      style={{ minWidth: "130px" }}
+                      style={{ whiteSpace: "nowrap" }}
                     >
                       {card.rep > 0
                         ? "🍒".repeat(Math.min(5, Math.round(card.rep)))
